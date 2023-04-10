49ad078bd17cfb616c4f3e87e29e38aa99f2f07c
syfa29faa <2006133@itg.ac.id>
2023-04-10 13:20:28 +0700

    menambahkan news

--- lib/modul/menu/menu_screen.dart
+++ lib/modul/menu/menu_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:navigator_apps/data/news_data.dart';
+import 'package:navigator_apps/models/news.dart';
+import 'package:navigator_apps/widget/news_item_widget.dart';
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Menu Screen"));
+    return ListView(
+      children: [
+        const ListTile(title: Text("News Article List")),
+        Column(
+          children:
+              newsData.map((News news) => NewsItemWidget(news: news)).toList(),
+        )
+      ],
+    );
   }
 }
