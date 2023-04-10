12d78534d44fbf2ae59757816bbcc4efcb1356d4
putrizakiyahlestari <2006142@itg.ac.id>
2023-04-10 12:18:52 +0700

    tambah file news detail

--- lib/modul/news_detail/news_detail_screen.dart
+++ lib/modul/news_detail/news_detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -9,6 +10,24 @@ class NewsDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("News Detail")),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(children: [
+            Image.network(news!.image!), 
+            Text(news!.title!, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+            Text(news!.description!, textAlign: TextAlign.justify,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [IconButton(onPressed: (){}, icon: Icon(Icons.favorite, color: Colors.red,)),
+            IconButton(onPressed: (){}, icon: Icon(Icons.message_outlined, color: Colors.amber,)),
+            IconButton(onPressed: (){}, icon: Icon(Icons.send_sharp, color: Colors.black,))
+            ],)
+          ]),
+        ),
+      ),
+    );
   }
 }
